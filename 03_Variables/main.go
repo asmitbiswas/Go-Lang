17c086025a83164cfb5463aced76a9d415f4b964
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-07 16:31:07 +0530

    feat: Variables

--- 03_Variables/main.go
+++ 03_Variables/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Asmit"
-	fmt.Println(name)
+	// var name string = "Asmit"
+	// infer
+	// var name = "Asmit"
+	// var isPassed bool = true
+	var age int = 36
+	fmt.Println(age)
 }
