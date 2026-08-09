612179847a2578b7ad98f1f31f19ca30223b769f
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-09 21:50:05 +0530

    Updated Constant example

--- 04_Constant/main.go
+++ 04_Constant/main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
+// const age = 45 // works here top
+// age := 141 /not works here \
+
 func main() {
 	// const passKey string = "53***********"
 
+	const age = 45
+
+	fmt.Println(age)
 }
