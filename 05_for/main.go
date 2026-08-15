3f0da202bf7ef61f72398d15f7bf211a4ee1001c
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-15 16:58:57 +0530

    done for,  infinte, classical for loop

--- 05_for/main.go
+++ 05_for/main.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
+	// i := 1
 
-	for i <= 3 {
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	/* infinte loop */
+	// for{
+	// 	fmt.Println("12")
+	// }
+
+	// classic loop
+
+	for i:=0; i< 5; i++{
+		
+		if i <= 2 {
+			continue
+		}
 		fmt.Println(i)
-		i = i + 1
 	}
 }
