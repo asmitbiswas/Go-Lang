ddcad9e72846bba150654d8638e208c0fa48db63
asmitbiswas <asmitbiswas1308@gmail.com>
2026-08-15 17:26:59 +0530

    if-else

---
+++ 06_if_else/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	// age:= 15
+
+	// if age >= 18 {
+	// 	fmt.Println("Person is an adult")
+	// } else{
+	// 	fmt.Println("Person isn't an adult")
+	// }
+
+	age:= 112
+
+	if  age >= 18 {
+		fmt.Println("Person is a adult")
+	} else if age >= 12 {
+		fmt.Println("Person is not an adult")
+	} else {
+		fmt.Println("Peson is a kid")
+	}
+}
